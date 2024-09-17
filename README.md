d7978df92fc60c298f55ec256819711ec203a6bb
James <james@jameskr.dev>
2024-09-17 03:20:30 +0000

    Initial Commit
    Includes all python files to start writing a discord bot.

---
+++ README.md
@@ -0,0 +1,14 @@
+# DiscordBotTemplate
+This is a application meant to be run inside a container that has all required packages installed to run the discord.py library.
+
+## Getting Started
+
+Run the following:
+```bash
+cp .env.sample .env
+```
+
+This will make a copy of .env, and edit the environment variables to be the correct values.
+`.env` is in `.gitignore`, so when you save your progress with git, the secrets in
+this file won't be saved.
+
---
+++ src/main.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import discord
+import os
+
+load_dotenv() # Load variables inside .env
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.environ.get('DISCORD_TOKEN'))
